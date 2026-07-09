8698e3ac0fa0534f388ee296346412218374f443
neprozrachno <neprozrachno@gmail.com>
2026-07-09 03:02:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.filters import CommandStart
@@ -13,7 +14,27 @@ from aiogram.types import (
     Message,
 )
 
-from config import ADMIN_ID, BOT_TOKEN, GROUP_LINK, RULES_TEXT, WISHLIST_LINK
+# ==================== НАСТРОЙКИ БОТА ====================
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8790030605:AAHotRsdKnlTNDREztgCyNCysx8UZzP1xxk")
+
+GROUP_LINK = os.getenv("GROUP_LINK", "https://t.me/+FOmfopugnRJlY2Zi")
+
+WISHLIST_LINK = os.getenv("WISHLIST_LINK", "https://web-production-a6c04.up.railway.app/wishlists/11/")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "1857155854"))
+
+RULES_TEXT = (
+    "<b>правила нах:</b>\n"
+    "1. блевать запрещено!! ⛔️ \n"
+    "2. драться тоже запрещено \n"
+    "3. ломать чето тоже пожалуйста не надо \n"
+    "4. с собой обязательно хорошее настроение!!!!!😍😍😍😍 \n"
+    "5. ээээ"
+    "6. чёто еще"
+    "7. итд"
+)
+# ==========================================================
 
 logging.basicConfig(level=logging.INFO)
 
